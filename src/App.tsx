51a0a7a0117f9cc20c122d3f486e98a2ad98b1b2
Prohor <prohor-yakovlev@mail.ru>
2022-12-20 18:18:21 +0300

    Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent, useState} from 'react';
 import './App.css';
+import Fullinput from "./components/Fullinput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    )
+
+    const addMessage = () => {
+        console.log("aaaaaaaa")
+    }
+
+    return (
+        <div className="App">
+            {/*<div>*/}
+            {/*  <input />*/}
+            {/*  <button>+</button>*/}
+            {/*</div>*/}
+
+            <Fullinput addMessage={addMessage}/>
+
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Fullinput.tsx
@@ -0,0 +1,26 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type FullInputPropsType = {
+    addMessage: (title: string)=> void
+}
+
+export const Fullinput = (props: FullInputPropsType) => {
+    let [title, setTitle] = useState('')
+
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+
+        setTitle(event.currentTarget.value)
+    }
+    const onChangeButtonHandler = ()=> {
+        props.addMessage(title)
+    }
+
+    return (
+        <div>
+            <input onChange={onChangeInputHandler}/>
+            <button onClick={onChangeButtonHandler}>+</button>
+        </div>
+    );
+};
+
+export default Fullinput;
